c74ec0efe92c1cdde5e389bf9be7160541564a6d
mmudov <milen.mudov@gmail.com>
2024-11-16 11:52:50 +0200

    - Updating the shopping cart so that it persists itself as session data.
    - Creating a shopping cart summary widget using a view component.
    - Receiving and validating user data.
    - Displaying data validation errors to the user

--- SportsStore/Models/Cart.cs
+++ SportsStore/Models/Cart.cs
@@ -3,7 +3,7 @@
     public class Cart
     {
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
-        public void AddItem(Product product, int quantity)
+        public virtual void AddItem(Product product, int quantity)//public void AddItem(Product product, int quantity)
         {
             CartLine? line = Lines
             .Where(p => p.Product.ProductID == product.ProductID)
@@ -22,9 +22,9 @@
                 line.Quantity += quantity;
             }
         }
-        public void RemoveLine(Product product) => Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
+        /*public void RemoveLine(Product product)*/public virtual void RemoveLine(Product product) => Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
         public decimal ComputeTotalValue() => Lines.Sum(e => e.Product.Price * e.Quantity);
-        public void Clear() => Lines.Clear();
+        /*public void Clear()*/public virtual void Clear() => Lines.Clear();
     }
     public class CartLine {
         public int CartLineID { get; set; }
--- SportsStore/obj/Debug/net9.0/SportsStore.AssemblyInfo.cs
+++ SportsStore/obj/Debug/net9.0/SportsStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61f615b0666fcdf504c46c36382487bcc41b141")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
